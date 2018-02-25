190f67ac099125c1596bbdb32d446d0cb744e975
Joalcapa <josecaceres.oreul@gmail.com>
2018-02-24 20:10:39 -0400

    Initialization of the view handler

---
+++ src/GUI/handlerGui.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import static domino.scores.DominoScores.pruebas;
+import domino.scores.Principal;
+import GUI.Views.Welcome;
+
+/**
+ *
+ * @author Joalcapa
+ */
+public class handlerGui {
+    
+     public static void main(String[] args) {
+         Welcome welcome = new Welcome();
+     }
+     
+}
